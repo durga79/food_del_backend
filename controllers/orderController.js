b19a8fb4212f497cf100239ed75ff9c8d868da81
Durga prasad <neiturp1637@gmail.com>
2024-08-18 13:24:25 +0530

    dh

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -58,7 +58,7 @@ const placeOrder=async (req,res)=>{
     
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:"Error"})
+        res.json({success:false,message:`${error}`})
     }
 }
 
